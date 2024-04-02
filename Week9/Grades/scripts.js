979d10dd8e7c88171b60e5ffd616d40de4aca4cc
Philip Dunne <philip.m.dunne@gmail.com>
2024-04-02 22:17:44 +0100

    Added Bonus feature to determine and display grade

--- Week9/Grades/scripts.js
+++ Week9/Grades/scripts.js
@@ -10,11 +10,41 @@ function subjectsAndGrades() {
     {   
 
         subjects[i] = prompt("Enter subject number " + i + " (numeric values only)");
-        results[i] = prompt("Enter result number " + i + " (numeric values only)");
-        document.getElementById("resultsOutput").innerHTML += (subjects[i] + ": " + results[i] + "<br/>");
+        results[i] = prompt("Enter result " + i + " (numeric values only, 0-100)");
+        document.getElementById("resultsOutput").innerHTML += ("Subject: " + subjects[i] + " | Result: " + results[i] + " | Grade: " + determineGrade(results[i]) + "<br/>");
         
     }
-        
+    
+    function determineGrade(result)
+    {
+        grade=0;
+        if(result < 20)
+        {
+            grade="F";
+        }
+        else if(result >=20 && result <40)
+        {
+            grade="E";
+        }
+        else if(result >=40 && result <50)
+        {
+            grade="D";
+        }
+        else if(result >=50 && result <70)
+        {
+            grade="C";
+        }
+        else if(result >=70 && result <85)
+        {
+            grade="B";
+        }
+        else if(result >=85 && result <=100)
+        {
+            grade="A";
+        }
+        return grade;
+    }
+
     function hideButton(){
         document.getElementById("subjectsAndGrades").style.display="none";
     }
